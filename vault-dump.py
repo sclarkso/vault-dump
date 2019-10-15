2f46b566cfd1339930765b61d31d3cf414698adb
Steven Clarkson <sclarkso@redhat.com>
2019-10-16 08:51:41 +1000

    added ability to specify mount point

--- vault-dump.py
+++ vault-dump.py
@@ -1,112 +1,90 @@
-#!/usr/bin/env python3
-
-import os
+#! /usr/bin/env python
+#
+# Dumps a HashiCorp Vault database to write statements.
+# Useful for backing up in-memory vault data
+# and later restoring from the generated script.
+#
+# Requires: an already-authenticated session
+#
+# Reads env vars:
+# - VAULT_ADDR  which points to desired Hashicorp Vault instance, default http://localhost:8200
+# - TOP_VAULT_PREFIX to specify path to dump, for partial backups, default /secret/
+# - MOUNT_POINT to vault mount point, default /
+#
+# Use custom encoding:
+#   PYTHONIOENCODING=utf-8 python vault-dump.py
+#
+# Copyright (c) 2017 Shane Ramey <shane.ramey@gmail.com> updated by Steven Clarkson <sclarkso@gmail.com>
+# Licensed under the Apache License, Version 2.0
+from __future__ import print_function
 import sys
-from pathlib import Path
-import getopt
-import requests
-import json
+import subprocess
+import os
+import pwd
+import hvac
 import datetime
 
+def print_header():
+    user = pwd.getpwuid(os.getuid()).pw_name
+    date = "{} UTC".format(datetime.datetime.utcnow())
+    vault_address = os.environ.get('VAULT_ADDR')
+    top_vault_prefix = os.environ.get('TOP_VAULT_PREFIX','/secret/')
+    mount_point = os.environ.get('MOUNT_POINT', '/')
 
-def usage():
-    print("usage: vault-dump.py [-a|--address URL] [-t|--token TOKEN]")
+    print ('#')
+    print ('# vault-dump.py backup')
+    print ("# dump made by {}".format(user))
+    print ("# backup date: {}".format(date))
+    print ("# VAULT_ADDR env variable: {}".format(vault_address))
+    print ("# TOP_VAULT_PREFIX env variable: {}".format(top_vault_prefix))
+    print ("# MOUNT_POINT env variable: {}".format(mount_point))
+    print ('# STDIN encoding: {}'.format(sys.stdin.encoding))
+    print ('# STDOUT encoding: {}'.format(sys.stdout.encoding))
+    print ('#')
+    print ('# WARNING: not guaranteed to be consistent!')
+    print ('#')
 
-
-def get_token():
-    if 'VAULT_TOKEN' in os.environ:
-        return os.environ['VAULT_TOKEN']
-
-    token_file = Path('/'.join([str(Path.home()), '.vault-token']))
-    if token_file.is_file():
-        with open(token_file) as f:
-            return f.read().strip()
-
-    raise Exception("There is no Vault token. Please set a token value in env var 'VAULT_TOKEN', "
-                    "the '~/.vault-token' file, or the '-t' arg.")
-
-
-def parse_opts():
-    address = None
-    token = None
-
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], 'ha:t:', ['help', 'address=', 'token='])
-    except getopt.GetoptError as err:
-        print(str(err))
-        sys.exit(2)
-
-    for opt, arg in opts:
-        if opt in ('-h', '--help'):
-            usage()
-            sys.exit()
-        if opt in ('-a', '--address'):
-            address = arg
-        if opt in ('-t', '--token'):
-            token = arg
-
-    if address is None:
-        address = os.environ.get('VAULT_ADDR', 'http://127.0.0.1:8200')
-
-    if token is None:
-        token = get_token()
-
-    return {'address': address, 'token': token}
-
-
-def vault_request(options, verb, path):
-    r = requests.request(method=verb, url=options['address']+'/v1/'+path, headers={'X-Vault-Token': options['token']})
-    if r.status_code != 200:
-        raise Exception("API request failed: {verb} {path}".format(verb=verb, path=path))
-    return json.loads(r.text)
-
-
-def check_vault_token(options):
-    try:
-        vault_request(options, 'GET', 'auth/token/lookup-self')
-    except Exception:
-        raise Exception("Failed to authenticate to Vault at {address}".format(address=options['address']))
-
-
-def fetch_kv2_secret(options, mount, path):
-    secret_path = mount + 'data/' + path
-    out_file = options['outdir']+mount+path+'.json'
-
-    print(mount+path)
-    secret = vault_request(options, 'GET', secret_path)
-
-    with open(out_file, 'w') as f:
-        json.dump(secret['data']['data'], f, indent=4)
-        f.write('\n')
-
-
-def list_kv2_secrets(options, mount, path=''):
-    list_path = mount + 'metadata/' + path
-    print(mount + path)
-
-    secrets = vault_request(options, 'LIST', list_path)
-
-    for element in secrets['data']['keys']:
-        if element[-1] == '/':
-            os.makedirs(options['outdir']+mount+path+element)
-            list_kv2_secrets(options, mount, path+element)
+# looks at an argument for a value and prints the key
+#  if a value exists
+def recurse_for_values(path_prefix, candidate_key):
+    candidate_values = candidate_key['data']['keys']
+    for candidate_value in candidate_values:
+        next_index = path_prefix + candidate_value
+        if candidate_value.endswith('/'):
+            next_value = client.secrets.kv.v2.list_secrets(next_index, mount_point=m1ount_point)
+            recurse_for_values(next_index, next_value)
         else:
-            fetch_kv2_secret(options, mount, path+element)
+            stripped_prefix=path_prefix[:-1]
+            final_dict = client.secrets.kv.v2.read_secret_version(next_index, mount_point=m1ount_point)['data']
+            print ("\nvault write {}".format(next_index), end='')
+
+            sorted_final_keys = sorted(final_dict.keys())
+            for final_key in sorted_final_keys:
+                final_value = final_dict[final_key]
+                try:
+                    final_value = final_value.encode("utf-8")
+                except AttributeError:
+                    final_value = final_value
+                print (" {0}={1}".format(final_key, repr(final_value)), end='')
 
 
-def main():
-    time = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+env_vars = os.environ.copy()
+hvac_token = subprocess.check_output(
+    "vault read -field id auth/token/lookup-self",
+    shell=True,
+    env=env_vars)
 
-    options = parse_opts()
-    options['outdir'] = str(Path.home()) + '/vault-dump/dumps/' + time + '/'
+hvac_url = os.environ.get('VAULT_ADDR','http://localhost:8200')
+hvac_client = {
+    'url': hvac_url,
+    'token': hvac_token,
+}
+client = hvac.Client(**hvac_client)
+assert client.is_authenticated()
 
-    print('Dumping contents of Vault server at '+options['address'])
-    print('Output directory is '+options['outdir'])
-    print()
+top_vault_prefix = os.environ.get('TOP_VAULT_PREFIX','/secret/')
+m1ount_point = os.environ.get('MOUNT_POINT','/')
 
-    check_vault_token(options)
-    list_kv2_secrets(options, 'secret/')
-
-
-if __name__ == "__main__":
-    main()
+print_header()
+top_level_keys = client.secrets.kv.v2.list_secrets(top_vault_prefix, mount_point=m1ount_point) 
+recurse_for_values(top_vault_prefix, top_level_keys)
